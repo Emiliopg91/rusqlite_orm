28c4c585c9f0414968e9128b05a932ddeed76f1b
Nils Höpfner <nils.van.wickeren@web.de>
2026-09-09 16:19:47 +0200

    Added From implementations for standard types for ValueOrFunction to enable seamless use

--- orm/src/types/value_or_function.rs
+++ orm/src/types/value_or_function.rs
@@ -38,3 +38,102 @@ impl Display for Function {
         )
     }
 }
+
+impl From<isize> for ValueOrFunction {
+    fn from(v: isize) -> Self {
+        ValueOrFunction::Value(Value::IntSize(v))
+    }
+}
+impl From<i8> for ValueOrFunction {
+    fn from(v: i8) -> Self {
+        ValueOrFunction::Value(Value::Int8(v))
+    }
+}
+impl From<i16> for ValueOrFunction {
+    fn from(v: i16) -> Self {
+        ValueOrFunction::Value(Value::Int16(v))
+    }
+}
+impl From<i32> for ValueOrFunction {
+    fn from(v: i32) -> Self {
+        ValueOrFunction::Value(Value::Int32(v))
+    }
+}
+impl From<i64> for ValueOrFunction {
+    fn from(v: i64) -> Self {
+        ValueOrFunction::Value(Value::Int64(v))
+    }
+}
+impl From<usize> for ValueOrFunction {
+    fn from(v: usize) -> Self {
+        ValueOrFunction::Value(Value::UntSize(v))
+    }
+}
+impl From<u8> for ValueOrFunction {
+    fn from(v: u8) -> Self {
+        ValueOrFunction::Value(Value::Unt8(v))
+    }
+}
+impl From<u16> for ValueOrFunction {
+    fn from(v: u16) -> Self {
+        ValueOrFunction::Value(Value::Unt16(v))
+    }
+}
+impl From<u32> for ValueOrFunction {
+    fn from(v: u32) -> Self {
+        ValueOrFunction::Value(Value::Unt32(v))
+    }
+}
+impl From<u64> for ValueOrFunction {
+    fn from(v: u64) -> Self {
+        ValueOrFunction::Value(Value::Unt64(v))
+    }
+}
+impl From<f32> for ValueOrFunction {
+    fn from(v: f32) -> Self {
+        ValueOrFunction::Value(Value::Float32(v))
+    }
+}
+impl From<f64> for ValueOrFunction {
+    fn from(v: f64) -> Self {
+        ValueOrFunction::Value(Value::Float64(v))
+    }
+}
+impl From<bool> for ValueOrFunction {
+    fn from(v: bool) -> Self {
+        ValueOrFunction::Value(Value::Bool(v))
+    }
+}
+impl From<String> for ValueOrFunction {
+    fn from(v: String) -> Self {
+        ValueOrFunction::Value(Value::Text(v))
+    }
+}
+impl From<&str> for ValueOrFunction {
+    fn from(v: &str) -> Self {
+        ValueOrFunction::Value(Value::Text(v.to_string()))
+    }
+}
+impl From<Vec<u8>> for ValueOrFunction {
+    fn from(v: Vec<u8>) -> Self {
+        ValueOrFunction::Value(Value::Blob(v))
+    }
+}
+
+impl From<&[u8]> for ValueOrFunction {
+    fn from(v: &[u8]) -> Self {
+        ValueOrFunction::Value(Value::Blob(v.to_vec()))
+    }
+}
+
+impl<T> From<Option<T>> for ValueOrFunction
+where
+    T: Into<ValueOrFunction>,
+{
+    fn from(opt: Option<T>) -> Self {
+        match opt {
+            Some(v) => v.into(),
+            None => ValueOrFunction::Value(Value::Null),
+        }
+    }
+}
